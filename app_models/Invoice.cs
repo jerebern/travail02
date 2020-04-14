6092f106a6eb752265d30a5d29975f71cf3a6578
Jérémy Bernard <jere.bern@hotmail.com>
2020-04-13 22:50:23 -0400

    3.2 Modèle Part 2

--- app_models/Invoice.cs
+++ app_models/Invoice.cs
@@ -15,26 +15,25 @@ namespace BillingManagement.Models
         private Double ProvTax;
         private Double Total;
 
-        public Invoice() {
+        public Invoice(double SubTotalTMP) {
             InvoiceId++;
             CreationDateTime = DateTime.Now;
             ProvTax = 0.9975;
             FedTax = 0.05;
-            SubTotal = 0;
-            ProvTax = 0;
-            Total = 0;
+            SubTotal = SubTotalTMP;
+            Total = (FedTax + ProvTax) * SubTotal;
 
         }
 
         public Invoice(Customer customerTMP)
         {
+            Customer = customerTMP;
             InvoiceId++;
             CreationDateTime = DateTime.Now;
             ProvTax = 0.9975;
             FedTax = 0.05;
-            SubTotal = 0;
-            ProvTax = 0;
-            Total = 0;
+            SubTotal = SubTotalTMP;
+            Total = (FedTax + ProvTax) * SubTotal;
 
         }
 
