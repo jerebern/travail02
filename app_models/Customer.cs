63592564fdb8c015c102490580af39df63af1122
Jérémy Bernard <jere.bern@hotmail.com>
2020-04-13 22:30:28 -0400

    Petite modification pour test de branche

--- app_models/Customer.cs
+++ app_models/Customer.cs
@@ -2,7 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+//TEST COMMIT
 namespace app_models
 {
     public class Customer : INotifyPropertyChanged
