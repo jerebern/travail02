e95227fe528a552e32cd7ec8f9a8009eaa4eea55
Jérémy Bernard <jere.bern@hotmail.com>
2020-04-13 22:44:08 -0400

    3.2 Modèle

---
+++ app_models/Invoice.cs
@@ -0,0 +1,42 @@
+﻿using app_models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingManagement.Models
+{
+    class Invoice
+    {
+        private static int InvoiceId = 0;
+        private DateTime CreationDateTime;
+        private Customer Customer;
+        private Double SubTotal;
+        private Double FedTax;
+        private Double ProvTax;
+        private Double Total;
+
+        public Invoice() {
+            InvoiceId++;
+            CreationDateTime = DateTime.Now;
+            ProvTax = 0.9975;
+            FedTax = 0.05;
+            SubTotal = 0;
+            ProvTax = 0;
+            Total = 0;
+
+        }
+
+        public Invoice(Customer customerTMP)
+        {
+            InvoiceId++;
+            CreationDateTime = DateTime.Now;
+            ProvTax = 0.9975;
+            FedTax = 0.05;
+            SubTotal = 0;
+            ProvTax = 0;
+            Total = 0;
+
+        }
+
+    }
+}
